cca07a8fe935fe079b30bed42397af28cff5f40c
fangzas05 <chayanad.fang222@gmail.com>
2024-11-10 01:18:09 +0700

    test

---
+++ test.dart
@@ -0,0 +1,17 @@
+void main(){
+ 
+  int x = 10, y = 3;
+  int q = (x~/y);
+  int r = x % y;
+  print(" quotient  ="" $q");
+  print(" remainder ="" $r"); 
+}
+
+
+echo "# dart-project 65310968 ชยณัฐ แก้วสว่าง" >> README.md
+git init
+git add *.*
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/fangzas05/dart-project.git
+git push -u origin main
