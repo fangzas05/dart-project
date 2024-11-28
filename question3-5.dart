ee7f2ac203bacf2894344125bf6316cb4730d643
fangzas05 <chayanad.fang222@gmail.com>
2024-11-28 22:07:16 +0700

    Add files via upload

---
+++ question3-5.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+import 'dart:math';
+void main(){
+print("กรุณาใส่เลขที่มากที่สุดในการ random");
+int? num = int.parse(stdin.readLineSync()!);
+Random random = new Random();
+int randomnum = random.nextInt(num);
+  print("เลขที่ได้ทำการ random :$randomnum");
+
+  }
